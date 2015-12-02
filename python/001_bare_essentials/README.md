b69ef8998250ccf5a74967ca9be3a30bbd195592
Trey Stout <treystout@gmail.com>
2015-12-02 14:58:45 -0500

    Adding first, super basic example in python

---
+++ python/001_bare_essentials/README.md
@@ -0,0 +1,21 @@
+Example 1
+===============
+
+This example demonstrates very simple API access to render a PDF of a message.
+It requires a valid API token pair, which can be created for free at
+https://handwriting.io
+
+After you have your token pair simply find the following two lines in
+ `example001.py`
+
+    API_TOKEN = "<YOUR TOKEN>"
+    API_SECRET = "<YOUR SECRET>"
+
+And change them to your token and secret. Be sure to keep the quotes, and don't include
+any spaces.
+
+Then, to run this script, simply invoke it...
+
+    $ python example001.py
+
+After it runs look for a file called `out.pdf` in the working directory and open it
---
+++ python/001_bare_essentials/example001.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""This script simply renders a message as a pdf and saves it to a file
+(out.pdf) in the working directory
+"""
+
+import sys
+
+try:
+  import requests
+except ImportError:
+  print "This script requires the requests library which can be downloaded at "\
+    "http://docs.python-requests.org/en/latest/"
+  sys.exit(1)
+
+BASE_URL = "https://api.handwriting.io"
+API_TOKEN = "<YOUR TOKEN>"
+API_SECRET = "<YOUR SECRET>"
+OUT_FILE = "out.pdf"
+
+# this is the message we will turn into handwriting. It supports most
+# white-space characters such as newline (\n) and tab (\t) as well as
+# regular spaces
+message_to_render = "Hey man!\n\n\tI love handwriting!\n\nLove,\n\tSomeone"
+
+# this is the actual rendering request for a PDF.
+r = requests.get("%s%s" % (BASE_URL, "/render/pdf"),
+    auth=(API_TOKEN, API_SECRET),
+    params={
+      'text': message_to_render,
+      'handwriting_id': '31SAZEF000DX', # find more ids at /handwritings
+      'handwriting_size': '15pt',
+      'height': '2in',
+      'width': '4in'
+    },
+    stream=True # to prevent in-memory buffering of the image
+  )
+r.raise_for_status()
+with open(OUT_FILE, 'wb') as f:
+  for chunk in r.iter_content(chunk_size=4096):
+    f.write(chunk)
+print "results written to %s" % OUT_FILE
